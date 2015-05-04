4bea0b8d596f0f28f3105cec495233e4b25e7f6b
Janine <janine.wenzel@griffithuni.edu.au>
2015-05-04 16:01:02 +0200

    bla

--- PodFeedCatchReader/src/itemSpeicher/ChannelVerzeichnisURL.java
+++ PodFeedCatchReader/src/itemSpeicher/ChannelVerzeichnisURL.java
@@ -8,6 +8,13 @@ public class ChannelVerzeichnisURL{
 	
 	ChannelVerzeichnisURL(){
 		liste = new LinkedList<ChannelURL>();
+		for(int i = 1; i < 10; ++i){
+			ChannelURL c = new ChannelURL("http://channel"+ i + ".de");
+			for(int j = 1; j < 4; ++j){
+				c.setFeed(new Item(j, ItemArt.Text, "Dies ist Item " + j + " von Channel" + i));
+			}
+			liste.add(c);
+		}
 	}
 
 	public LinkedList<ChannelURL> getChannels() {
