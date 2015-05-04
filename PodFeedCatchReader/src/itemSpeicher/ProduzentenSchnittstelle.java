2d62db5d01b3e0e45294ecffc1c8b96ca23f63b4
Janine <janine.wenzel@griffithuni.edu.au>
2015-05-04 14:02:40 +0200

    blub

---
+++ PodFeedCatchReader/src/itemSpeicher/ProduzentenSchnittstelle.java
@@ -0,0 +1,6 @@
+package itemSpeicher;
+
+public interface ProduzentenSchnittstelle {
+	boolean erstelleItem(ItemArt art, String inhalt, int ChannelID);
+	boolean erstelleChannel(String name);
+}
--- PodFeedCatchReader/src/itemSpeicher/ProduzentenSchnittstelleUni.java
+++ PodFeedCatchReader/src/itemSpeicher/ProduzentenSchnittstelleUni.java
@@ -1,7 +1,5 @@
 package itemSpeicher;
 
 
-public interface ProduzentenSchnittstelleUni {
-	boolean erstelleItem(ItemArt art, String inhalt, int ChannelID);
-	boolean erstelleChannel(String name);
+public interface ProduzentenSchnittstelleUni extends ProduzentenSchnittstelle{
 }
